a56ee5a657f1fcc97cda27519a44a50dd40df1d3
José Luiz <104479818+jldsn@users.noreply.github.com>
2025-01-31 16:59:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ const tarefaRoutes = require('./routes/tarefaRoutes');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Rotas
+app.use('/tarefas', tarefaRoutes);
+
+// Iniciar o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
