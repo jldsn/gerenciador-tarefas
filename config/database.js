d5ed8d3e425aa178a3c6791461da93670f99863e
José Luiz <104479818+jldsn@users.noreply.github.com>
2025-03-05 19:35:17 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/gerenciador_tarefas', {
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/gerenciador_tarefas';
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
