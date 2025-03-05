26d057ee381da5dd4fdb2f8fc7f6f6cdbdff2969
José Luiz <104479818+jldsn@users.noreply.github.com>
2025-03-05 20:29:14 -0300

    Update tarefaRoutes.js

--- routes/tarefaRoutes.js
+++ routes/tarefaRoutes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const tarefaController = require('../controllers/tarefaController');
 
-router.post('/', tarefaController.criarTarefa);
-router.get('/', tarefaController.listarTarefas);
-router.get('/:id', tarefaController.buscarTarefa);
-router.put('/:id', tarefaController.atualizarTarefa);
-router.delete('/:id', tarefaController.excluirTarefa);
+router.get('/', (req, res) => {
+  res.send('Bem-vindo ao Gerenciador de Tarefas! Acesse /tarefas para ver as tarefas.');
+});
+
+router.post('/tarefas', tarefaController.criarTarefa);
+router.get('/tarefas', tarefaController.listarTarefas);
+router.get('/tarefas/:id', tarefaController.buscarTarefa);
+router.put('/tarefas/:id', tarefaController.atualizarTarefa);
+router.delete('/tarefas/:id', tarefaController.excluirTarefa);
 
 module.exports = router;
