f0b2943280167aeb1b266c7171e3305656a20e2a
José Luiz <104479818+jldsn@users.noreply.github.com>
2025-03-05 19:50:26 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/gerenciador_tarefas';
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Conectado ao MongoDB'))
-.catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
 module.exports = mongoose;
